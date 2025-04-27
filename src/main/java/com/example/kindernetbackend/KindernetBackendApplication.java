9ed67101844b125ebaf0814e573e45b8daffd2c1
Ghada Bel <ghadabelhadj19@gmail.com>
2025-04-27 10:45:30 +0100

    Initial commit

---
+++ src/main/java/com/example/kindernetbackend/KindernetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kindernetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindernetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KindernetBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kindernetbackend/KindernetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kindernetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindernetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
